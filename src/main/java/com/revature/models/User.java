eb86f5d0a28a46c8c3e122f6c0335b0648d9d6d7
Tiffany Azamar <tiffanyazamar@tiffanysmacbook.lan>
2020-09-10 17:20:43 -0600

    Created user class

---
+++ src/main/java/com/revature/models/User.java
@@ -0,0 +1,81 @@
+package com.revature.models;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+import org.hibernate.annotations.ManyToAny;
+
+@Entity
+public class User implements Serializable{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int userID;
+	private String username;
+	private String password;
+	private String firstName;
+	private String lastName;
+	private String phoneNumber;
+	@ManyToOne(optional = false, fetch = FetchType.EAGER)
+	private UserRole userRole;
+	private Lease lease;
+	public int getUserID() {
+		return userID;
+	}
+	public void setUserID(int userID) {
+		this.userID = userID;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+	public UserRole getUserRole() {
+		return userRole;
+	}
+	public void setUserRole(UserRole userRole) {
+		this.userRole = userRole;
+	}
+	public Lease getLease() {
+		return lease;
+	}
+	public void setLease(Lease lease) {
+		this.lease = lease;
+	}
+	
+	
+}
