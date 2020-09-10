a4fdba1f41541011c6f93505433b6bd51e44f527
Laura <lauvillarreal@berkeley.edu>
2020-09-10 16:15:36 -0700

    added models-events,role,lease

--- src/main/java/com/revature/models/Role.java
+++ src/main/java/com/revature/models/Role.java
@@ -22,7 +22,7 @@ public class Role implements Serializable {
 	private static final long serialVersionUID = 1L;
 	@Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
-	@Column(name="ers_user_role_id", nullable=false)
+	@Column(name="user_role_id", nullable=false)
 	private int roleID;
 	@Column(name="user_role", nullable=false)
 	private String role;
@@ -30,5 +30,99 @@ public class Role implements Serializable {
 
 	@OneToMany(mappedBy="roleID", fetch=FetchType.LAZY)
 	private List<User> userList;
+
+
+	public Role(String role, List<User> userList) {
+		super();
+		this.role = role;
+		this.userList = userList;
+	}
+
+
+	public Role(int roleID, String role, List<User> userList) {
+		super();
+		this.roleID = roleID;
+		this.role = role;
+		this.userList = userList;
+	}
+
+
+	public Role() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+
+	public int getRoleID() {
+		return roleID;
+	}
+
+
+	public void setRoleID(int roleID) {
+		this.roleID = roleID;
+	}
+
+
+	public String getRole() {
+		return role;
+	}
+
+
+	public void setRole(String role) {
+		this.role = role;
+	}
+
+
+	public List<User> getUserList() {
+		return userList;
+	}
+
+
+	public void setUserList(List<User> userList) {
+		this.userList = userList;
+	}
+
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((role == null) ? 0 : role.hashCode());
+		result = prime * result + roleID;
+		return result;
+	}
+
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Role other = (Role) obj;
+		if (role == null) {
+			if (other.role != null)
+				return false;
+		} else if (!role.equals(other.role))
+			return false;
+		if (roleID != other.roleID)
+			return false;
+		return true;
+	}
+
+
+	@Override
+	public String toString() {
+		return "Role [roleID=" + roleID + ", role=" + role + "]";
+	}
+	
+	
 
 }
